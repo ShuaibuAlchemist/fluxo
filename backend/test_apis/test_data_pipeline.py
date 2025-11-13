79e1d730268914f2b7259f0e811a105d570a3b20
freemandaily <onahchinemelem@gmail.com>
2025-11-13 11:43:37 +0100

    added test api for onchain integrations and data pipeline

---
+++ backend/test_apis/test_data_pipeline.py
@@ -0,0 +1,44 @@
+import requests
+import time
+
+def test_portfolio():
+    
+    url = "http://0.0.0.0:8000/agent/portfolio/?address=0x5C30940A4544cA845272FE97c4A27F2ED2CD7B64"
+    response = requests.get(url)
+    print(response.status_code)
+    time.sleep(30)
+
+    # task= response.json()
+    # url = f"http://0.0.0.0:8000/agent/portfolio/status/task/{task['task_id']}"
+    # response = requests.get(url)
+    # print(response.json())
+    assert response.status_code == 200
+
+# fetch all the yield on mantle protocol
+def test_mantle_protocols_yield(): 
+    url = "http://0.0.0.0:8000/agent/yield"
+    response = requests.get(url)
+    print(response.status_code)
+
+    task = response.json()
+    if response.status_code == 200:
+        time.sleep(60*2) # allow background task to finish
+
+        url = f"http://0.0.0.0:8000/agent/yield/status/{task['task_id']}"
+        response = requests.get(url)
+        result = response.json()
+
+# fetch all the protocol on mantle
+def test_mantle_protocols():
+    url = "http://0.0.0.0:8000/agent/protocols"
+    response = requests.get(url)
+    print(response.status_code)
+
+    task = response.json()
+    if response.status_code == 200:
+        time.sleep(60*2) # allow background task to finish
+
+        url = f"http://0.0.0.0:8000/agent/protocols/status/{task['task_id']}"
+        response = requests.get(url)
+        result = response.json()
+    
